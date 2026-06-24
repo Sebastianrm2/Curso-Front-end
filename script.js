beeca35b951b465a9e81c37915c3b8a9c790d59c
Sebastian Rincon <sebastian2016rincon@gmail.com>
2026-06-24 17:33:07 -0500

    Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+const miNombre = "Sebastian";
+
+const miEdad = 15;
+
+let aprendiendoFrontend = true;
+
+const tecnologias = ["Javascript","Css","Html","Python"];
+
+const desarrollador = {
+
+    nombre: "Sebastian",
+
+    nivel: "Junior"
+
+};
+
+console.log(miNombre);
+console.log(miEdad);
+console.log(aprendiendoFrontend);
+console.log(tecnologias);
+console.log(desarrollador);
