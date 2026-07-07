d2f64a0ab2ec0f8e0c4657f87d3abab4ed1c7ac8
Sebastian Rincon <sebastian2016rincon@gmail.com>
2026-07-06 22:55:52 -0500

    Funcione

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ console.log(miEdad);
 console.log(aprendiendoFrontend);
 console.log(tecnologias);
 console.log(desarrollador);
+
+
+En tu script.js, crea estas 3 funciones usando arrow functions:
+
+const saludar = (nombre) => {
+    return "Hola " + nombre;
+} 
+
+console.log(saludar("Sebastian"));
+
+const sumar = (a, b) => {
+    return a + b;
+} 
+console.log(sumar(10, 5));
+
+const describir = (objeto) => {
+    
+    return objeto.nombre + " es nivel " + objeto.nivel
+}
+
+console.log(describir({ nombre: "Sebastian", nivel: "Junior" }));
+
