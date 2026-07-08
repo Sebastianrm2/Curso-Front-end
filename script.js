77c278307dc73819697ec0b7d9e0a61ad918cdf7
Sebastian Rincon <sebastian2016rincon@gmail.com>
2026-07-07 23:24:51 -0500

    script

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
-const miNombre = "Sebastian";
-
-const miEdad = 15;
-
-let aprendiendoFrontend = true;
-
-const tecnologias = ["Javascript","Css","Html","Python"];
-
-const desarrollador = {
-
-    nombre: "Sebastian",
-
-    nivel: "Junior"
-
-};
-
-console.log(miNombre);
-console.log(miEdad);
-console.log(aprendiendoFrontend);
-console.log(tecnologias);
-console.log(desarrollador);
 
 
-En tu script.js, crea estas 3 funciones usando arrow functions:
+const boton = document.getElementById("btn-saludo");
 
-const saludar = (nombre) => {
-    return "Hola " + nombre;
-} 
+const mensaje = document.getElementById("mensaje");
 
-console.log(saludar("Sebastian"));
-
-const sumar = (a, b) => {
-    return a + b;
-} 
-console.log(sumar(10, 5));
-
-const describir = (objeto) => {
-    
-    return objeto.nombre + " es nivel " + objeto.nivel
-}
-
-console.log(describir({ nombre: "Sebastian", nivel: "Junior" }));
-
-const crearTarjeta = (objeto) => {
-    return "Proyecto " + objeto.titulo + " | tecnologias: " + objeto.tecnologias 
-}
-
-console.log(crearTarjeta({titulo: "Aprendiendo las siguientes Tecnologias", tecnologias:"Javascript"}));
+boton.addEventListener("click", () => {
+    mensaje.textContent = "Hola Sebastian, bienvenido al DOM!";
+});
