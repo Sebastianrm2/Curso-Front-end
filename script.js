1feac3dcc4774c3087f40824f11a19654ff69e81
Sebastian Rincon <sebastian2016rincon@gmail.com>
2026-07-08 23:15:05 -0500

    IF

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ botonSaludar.addEventListener("click", () => {
 botonLimpiar.addEventListener("click", () => {
     mensaje.textContent = "";
 });
+
+    const inputNombre = document.getElementById("input-nombre");
+
+    const botonVerificar = document.getElementById("btn-verificar");
+
+    const texto = document.getElementById("resultado-nombre");
+
+    botonVerificar.addEventListener("click", () => {
+
+    const valor = inputNombre.value;
+
+        if ( valor === "" ){
+            texto.textContent = "Por favor escribe tu nombre";
+    }else{
+            texto.textContent = "Hola, " + valor;
+    }
+        
+    });
+
